1d396ee4a2eac5f8732893adf7835847c006bfb6
NooobtimeX <nooobtimex@gmail.com>
2026-02-11 08:14:19 +0700

    chore: prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import js from '@eslint/js'
 import nextPlugin from '@next/eslint-plugin-next'
-import reactPlugin from 'eslint-plugin-react'
 import hooksPlugin from 'eslint-plugin-react-hooks'
 import tseslint from 'typescript-eslint'
 
@@ -14,21 +13,12 @@ export default tseslint.config(
 		files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
 		plugins: {
 			'@next/next': nextPlugin,
-			'react': reactPlugin,
 			'react-hooks': hooksPlugin
 		},
 		rules: {
 			...nextPlugin.configs.recommended.rules,
 			...nextPlugin.configs['core-web-vitals'].rules,
-			...reactPlugin.configs.flat.recommended.rules,
-			...hooksPlugin.configs.recommended.rules,
-			'react/react-in-jsx-scope': 'off',
-			'react/prop-types': 'off' // often not needed with TS
-		},
-		settings: {
-			react: {
-				version: 'detect'
-			}
+			...hooksPlugin.configs.recommended.rules
 		}
 	}
 )
