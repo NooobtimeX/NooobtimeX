1e8bda1bc1e17e3c3d6655e20c91a66e3311497f
NooobtimeX <nooobtimex@gmail.com>
2026-03-26 23:03:54 +0700

    feat: poker photo

--- common/data/issue/online-poker-game.ts
+++ common/data/issue/online-poker-game.ts
@@ -1,4 +1,4 @@
-import { nextjs, postgresql, prisma, render, sse, tailwindcss, websocket } from '@/common/data/ability'
+import { nextjs, postgresql, prisma, render, sse, tailwindcss } from '@/common/data/ability'
 import { AffiliationId } from '@/common/enum'
 import type { Issue } from '@/common/interface'
 
@@ -6,13 +6,13 @@ export const onlinePokerGame: Issue = {
 	id: 'online-poker-game',
 	title: 'Online Poker Game',
 	description:
-		'A real-time multiplayer online poker game featuring WebSocket-based gameplay and Server-Sent Events for live updates. Built with Next.js and PostgreSQL for a seamless, interactive card gaming experience.',
+		'A real-time multiplayer online poker game featuring Server-Sent Events for live updates. Built with Next.js and PostgreSQL for a seamless, interactive card gaming experience.',
 	images: {
-		thumbnail: '/issue/online-poker-game.png',
-		banner: '/issue/online-poker-game.png',
-		photos: ['/issue/online-poker-game.png']
+		thumbnail: '/issue/poker/Picture1.png',
+		banner: '/issue/poker/Picture2.png',
+		photos: ['/issue/poker/Picture1.png', '/issue/poker/Picture2.png', '/issue/poker/Picture3.png']
 	},
-	abilities: [nextjs, prisma, render, tailwindcss, websocket, sse, postgresql],
+	abilities: [nextjs, prisma, render, tailwindcss, sse, postgresql],
 	links: {},
 	startDate: '2025-03-01',
 	linkedAffiliationId: AffiliationId.FreelanceBlitzwerk
